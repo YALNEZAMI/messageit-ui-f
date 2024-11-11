38854cca89b159c51987e346c26e240f7fbb6012
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-11 18:07:59 +0100

    init component

--- components/notification/index.vue
+++ components/notification/index.vue
@@ -1,9 +1,10 @@
 <template>
-  <main class="bg-black text-white">{{ notification.name }}</main>
+  <main class="bg-black text-white">{{ notification.sender.name }}</main>
 </template>
 <script setup>
 const props = defineProps({
   notification: Object,
 });
 const notification = props.notification;
+console.log("notifications", notification);
 </script>
