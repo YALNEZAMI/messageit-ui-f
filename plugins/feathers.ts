d547d6158cd7f473e9fe31387a2f78248dfe391f
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-11 18:10:28 +0100

    refact: use socket io in feathers client

--- plugins/feathers.ts
+++ plugins/feathers.ts
@@ -1,18 +1,33 @@
 // plugins/feathers.js
 import { feathers } from "@feathersjs/feathers";
-import rest from "@feathersjs/rest-client";
-import axios from "axios";
-
+import socketio from "@feathersjs/socketio-client";
+import io from "socket.io-client";
+// import rest from "@feathersjs/rest-client";
+// import axios from "axios";
 export default defineNuxtPlugin(() => {
-  const feathersClient = feathers();
-  const config = useRuntimeConfig();
-  // Configure the REST client
-  const restClient = rest(config.public.FEATHERS_URL);
-  feathersClient.configure(restClient.axios(axios));
+  const apiUrl = useRuntimeConfig().public.FEATHERS_URL;
+  const authStore = useAuthStore();
+  authStore.initializeAuth();
+  const token = authStore.accessToken;
+
+  let socket;
+  if (authStore.isAuthenticated()) {
+    socket = io(apiUrl, {
+      extraHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  } else {
+    socket = io(apiUrl);
+  }
+  // Remplacez par l'URL de votre serveur FeathersJS
+  const app = feathers();
+
+  app.configure(socketio(socket));
 
   return {
     provide: {
-      feathers: feathersClient,
+      feathers: app,
     },
   };
 });
