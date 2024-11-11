be0940a7edd0aecaed385844008fef5c1df60bb2
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-11 18:09:34 +0100

    init interface

---
+++ interfaces/notification.ts
@@ -0,0 +1,8 @@
+import type { User } from "./user";
+
+export interface Notification {
+  _id: string;
+  sender: User | string;
+  recipient: User | string;
+  type: "friendRequest" | "friendAcceptation";
+}
