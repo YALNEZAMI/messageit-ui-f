d39869fd0e491d1a85582d5db94386ae4199e16c
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-11 18:12:18 +0100

    style: add spiner to submit button

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -66,10 +66,32 @@
       <button
         type="button"
         @click="register"
-        class="bg-blue-500 min-w-20 hover:bg-blue-600 cursor-pointer transition-all duration-500 p-1 px-2 rounded text-white my-2 font-bold"
+        class="bg-indigo-500 min-w-20 hover:bg-indigo-600 cursor-pointer transition-all duration-500 p-1 px-2 rounded text-white my-2 font-bold"
       >
-        <span v-if="!loading">S'inscrire</span>
-        <span v-else>...</span>
+        <span v-if="!loading">S'inscrir</span>
+        <span v-else class="flex items-center">
+          <svg
+            class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              class="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              stroke-width="4"
+            ></circle>
+            <path
+              class="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          Inscription
+        </span>
       </button>
     </div>
     <div class="text-center">
@@ -137,6 +159,7 @@ const register = async () => {
     lanceAlert("Format invalide de votre adresse mail");
   }
   const res = await authStore.register(auth.value);
+
   if (res._id) {
     authStore.login({
       email: auth.value.email,
