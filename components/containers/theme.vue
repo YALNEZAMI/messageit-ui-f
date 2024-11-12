28d2b9253d90374ee510ce24c57ca016eb2823aa
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-12 01:44:49 +0100

    add themes

--- components/containers/theme.vue
+++ components/containers/theme.vue
@@ -2,7 +2,10 @@
 <template>
   <div
     :class="{
-      'bg-blue-300': useAuthStore().user.theme == 'basic',
+      'bg-blue-400 text-white': useAuthStore().user.theme._id == 'basic',
+      'bg-pink-400 text-white': useAuthStore().user.theme._id == 'love',
+      'bg-black text-white': useAuthStore().user.theme._id == 'panda',
+      'bg-green-400 text-white': useAuthStore().user.theme._id == 'spring',
     }"
     v-bind="attrs"
   >
