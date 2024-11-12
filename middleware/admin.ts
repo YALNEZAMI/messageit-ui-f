0a33b3c4b8df504cff573f0c513284e1da0ba991
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-12 22:27:22 +0100

    remove socket listner unefficient calls

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,6 +1,4 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
-  await useFriendsStore().onFriendRequestCreated();
-  await useFriendsStore().onAcceptationCreated();
 });
