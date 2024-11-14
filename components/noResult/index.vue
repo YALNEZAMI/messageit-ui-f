95bf25b1a9d1cb285ec5340893e3beef661a984a
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-14 13:21:38 +0100

    init noResult component

---
+++ components/noResult/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main class="bg-red-400 p-1 text-white h-max w-full text-center font-serif">
+    {{ props.message }}
+  </main>
+</template>
+<script setup>
+const props = defineProps({
+  message: String,
+});
+</script>
