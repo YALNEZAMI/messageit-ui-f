5fc993fbb214a75c805afebd3d8fc1e08d4260d5
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-14 19:29:50 +0100

    make theme required

--- components/containers/theme.vue
+++ components/containers/theme.vue
@@ -2,10 +2,10 @@
 <template>
   <div
     :class="{
-      'bg-blue-400 text-white': useAuthStore().user.theme._id == 'basic',
-      'bg-pink-400 text-white': useAuthStore().user.theme._id == 'love',
-      'bg-black text-white': useAuthStore().user.theme._id == 'panda',
-      'bg-green-400 text-white': useAuthStore().user.theme._id == 'spring',
+      'bg-blue-400 text-white': useAuthStore().user.theme?._id == 'basic',
+      'bg-pink-400 text-white': useAuthStore().user.theme?._id == 'love',
+      'bg-black text-white': useAuthStore().user.theme?._id == 'panda',
+      'bg-green-400 text-white': useAuthStore().user.theme?._id == 'spring',
     }"
     v-bind="attrs"
   >
