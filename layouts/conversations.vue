54624f7055e75f8a67c8d06207ef1923c51f32f3
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-14 15:10:41 +0100

    init conversation layout

---
+++ layouts/conversations.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div>
+      <ConversationHeader></ConversationHeader>
+      <ConversationNavBar></ConversationNavBar>
+    </div>
+
+    <div><slot /></div>
+  </main>
+</template>
+<script lang="ts" setup>
+definePageMeta({
+  middleware: "conversations",
+});
+</script>
