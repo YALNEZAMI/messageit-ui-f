287ac3db0a83e8ec67f098e22765e1bf879cd34c
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-14 19:33:21 +0100

    implement chat button logic

--- stores/conversations.store.ts
+++ stores/conversations.store.ts
@@ -1,7 +1,6 @@
 import { defineStore } from "pinia";
 import type { Conversation } from "~/interfaces/conversation";
-import conversations from "~/middleware/conversations";
-export const useStore = defineStore("store", {
+export const useConversationsStore = defineStore("conversationsStore", {
   state: () => {
     return {
       conversations: [] as Conversation[],
@@ -14,13 +13,43 @@ export const useStore = defineStore("store", {
     getService(name: string) {
       return useNuxtApp().$feathers.service(name);
     },
-    async conversationExistWith(idUser: string) {
-      return await this.getService("members").get("", {
+    async chatWithUser(idUser: string) {
+      const conv = await this.getService("conversations").get("", {
         query: {
           user1: idUser,
           user2: useAuthStore().user._id,
         },
       });
+
+      useRouter().push(`/private-space/conversations/${conv._id}`);
     },
+    //if bool is true id is set as Conversation _id, null otherwise
+    // async conversationExistWith(
+    //   idUser: string
+    // ): Promise<any> // : Promise<{ bool: boolean; _id: string }>
+    // {
+    //   const myId = useAuthStore().user._id;
+    //   const response = await this.getService("conversations").get("", {
+    //     query: {
+    //       user1: myId,
+    //       user2: idUser,
+    //     },
+    //   });
+    //   console.log("response", response);
+
+    //   // response1.concact(response2);
+    //   // console.log("response", merge);
+    //   // if (merge.length == 0) {
+    //   //   return {
+    //   //     bool: false,
+    //   //     _id: "",
+    //   //   };
+    //   // } else {
+    //   //   return {
+    //   //     bool: true,
+    //   //     _id: merge[0]._id,
+    //   //   };
+    //   // }
+    // },
   },
 });
