7fcbbf1c2b62edd45d4d682503b2d39141c9936b
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-14 19:29:08 +0100

    enable pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  pages: true,
   ssr: false,
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
