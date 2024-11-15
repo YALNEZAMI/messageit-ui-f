cff220cb2fa6ec3411087a11a24697d77538c25b
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-15 20:37:03 +0100

    call websocket listener

--- middleware/conversations.ts
+++ middleware/conversations.ts
@@ -1,4 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
+  useConversationsStore().onConversation();
 });
