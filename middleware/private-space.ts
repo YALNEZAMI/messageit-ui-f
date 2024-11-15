c10209e80285f1b2e446ef6c63374a9e891b832e
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-15 20:36:06 +0100

    call websockets listners

--- middleware/private-space.ts
+++ middleware/private-space.ts
@@ -1,4 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
+  await useFriendsStore().onFriendRequests();
+  await useFriendsStore().onFriends();
 });
