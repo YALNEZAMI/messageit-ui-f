b5fc72b855ebf69f37d2a6685c18b2ab8f3e1f55
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-16 15:13:20 +0100

    delete container to separate user theme and conversation theme handling in nav bar items color

--- components/navBarItem/index.vue
+++ components/navBarItem/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <ContainersTheme
+  <div
     @click="goTo()"
-    class="hover:opacity-70 cursor-pointer flex sm:min-w-28 items-center justify-center sm:space-x-2 p-1 px-3 sm:px-1 sm:pr-3 m-1 sm:mx-2 rounded transition-all duration-500"
+    class="hover:opacity-70 cursor-pointer flex sm:min-w-28 items-center justify-center sm:space-x-2 px-2 sm:px-1 sm:pr-3 m-1 sm:mx-2 rounded transition-all duration-500"
   >
     <div class="hidden sm:block">
       {{ navBarItem.name }}
@@ -29,7 +29,7 @@
         hidden: !isCurrentRoute(),
       }"
     ></div>
-  </ContainersTheme>
+  </div>
 </template>
 <script setup>
 // import { NavBarItem } from "../../interfaces/navBarItem";
