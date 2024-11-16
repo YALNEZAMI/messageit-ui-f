78a60ed734680d1a59e6f51e64e4b91577888a91
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-16 15:10:45 +0100

    init conversation theme container

---
+++ components/containers/conversationTheme.vue
@@ -0,0 +1,22 @@
+<!-- components/ThemeContainer.vue -->
+<template>
+  <div
+    :class="{
+      'bg-blue-400 text-white':
+        useConversationsStore().currentConversation.theme?._id == 'basic',
+      'bg-pink-400 text-white':
+        useConversationsStore().currentConversation.theme?._id == 'love',
+      'bg-black text-white':
+        useConversationsStore().currentConversation.theme?._id == 'panda',
+      'bg-green-400 text-white':
+        useConversationsStore().currentConversation.theme?._id == 'spring',
+    }"
+    v-bind="attrs"
+  >
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const attrs = useAttrs();
+</script>
