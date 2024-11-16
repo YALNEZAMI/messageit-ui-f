fd6e2d4d832aa12673166c48b35006f68a63d83a
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-16 15:15:08 +0100

    refact interface types

--- interfaces/conversation.ts
+++ interfaces/conversation.ts
@@ -1,7 +1,10 @@
 import type { Theme } from "./theme";
+import type { User } from "./user";
 
 export interface Conversation {
   _id?: string;
   name?: string;
   theme: Theme;
+  user1: string | User;
+  user2: string | User;
 }
