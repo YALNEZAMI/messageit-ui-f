b09d9557d0c2d3bc5510881ea0395e8b3ece7dc9
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-16 15:14:27 +0100

    init status component

---
+++ components/status/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main
+    class="w-2 h-2 rounded-full"
+    :class="{
+      'bg-gray-500': !user.onLine,
+      'bg-green-400': user.onLine,
+    }"
+  ></main>
+</template>
+<script setup>
+const props = defineProps({
+  user: "Object",
+});
+</script>
