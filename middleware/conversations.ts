fac57ea137c0ee6753fb930956dbe5dd529da308
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-16 15:15:39 +0100

    prevent errors if idConv is undefined (all conversations page case)

--- middleware/conversations.ts
+++ middleware/conversations.ts
@@ -1,5 +1,12 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
+  const idConv = useRoute().params.id as string;
+  if (idConv) {
+    const conv = await useConversationsStore().getConversation(idConv);
+    conv.user1 = await useUsersStore().getUser(conv.user1);
+    conv.user2 = await useUsersStore().getUser(conv.user2);
+    useConversationsStore().setCurrentConversation(conv);
+  }
   useConversationsStore().onConversation();
 });
