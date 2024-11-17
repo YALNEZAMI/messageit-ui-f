30e6b27598f05c9f22160cad618711d30eefe64a
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-17 17:26:39 +0100

    use id within the to object instead of useRoute() to ensure the id is present

--- middleware/conversations.ts
+++ middleware/conversations.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
   //set conversation
-  const idConv = useRoute().params.id as string;
+  const idConv = to.params.id as string;
   if (idConv) {
     const conv = await useConversationsStore().getConversation(idConv);
     useConversationsStore().setCurrentConversation(conv);
