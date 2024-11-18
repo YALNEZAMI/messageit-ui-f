c16ed3be7213389ac420650dd7345914d1d0f8ae
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-18 01:54:15 +0100

    delete hover effect as it is not effective

--- components/navBarItem/index.vue
+++ components/navBarItem/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     @click="goTo()"
-    class="hover:opacity-70 cursor-pointer flex md:min-w-20 lg:min-w-24 items-center justify-center sm:space-x-2 px-2 sm:px-1 sm:pr-3 m-1 sm:mx-2 rounded transition-all duration-500"
+    class="cursor-pointer flex md:min-w-20 lg:min-w-24 items-center justify-center sm:space-x-2 px-2 sm:px-1 sm:pr-3 m-1 sm:mx-2 rounded transition-all duration-500"
   >
     <div class="hidden md:block">
       {{ navBarItem.name }}
