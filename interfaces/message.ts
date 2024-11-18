351e642bf14a49e1b86b53acc12c9076c2514869
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-18 01:11:56 +0100

    init message interface

---
+++ interfaces/message.ts
@@ -0,0 +1,9 @@
+import type { Conversation } from "./conversation";
+import type { User } from "./user";
+
+export interface Message {
+  _id?: string;
+  sender: string | User;
+  conversation: string | Conversation;
+  text?: string;
+}
