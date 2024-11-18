4fbdf7b09289c23c62d63591acc496641e469d47
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-18 01:05:44 +0100

    init new compo

---
+++ components/images/userImage.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <NuxtImg class="w-full h-full" :src="imgSrc" @error="handleError"></NuxtImg>
+  </main>
+</template>
+
+<script setup>
+const props = defineProps({
+  src: String, // Fixed type definition
+});
+
+const authStore = useAuthStore();
+const defaultUserImg = authStore.defaultUserImg;
+
+const imgSrc = ref(props.src); // Local reactive variable for the image source
+
+const handleError = () => {
+  imgSrc.value = defaultUserImg; // Set to default image on error
+};
+</script>
