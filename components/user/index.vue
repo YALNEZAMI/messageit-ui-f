be76b71829a106f4fcafbe3347b70e992eb5f3bb
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-18 22:48:23 +0100

    no buttons on user when its the currentUser

--- components/user/index.vue
+++ components/user/index.vue
@@ -14,7 +14,7 @@
     <div class="mx-2 w-1/2 md:3/4 truncate">
       {{ user ? user.name.toUpperCase() : "Deconnecté" }}
     </div>
-    <div id="btngroupe">
+    <div id="btngroupe" v-if="user._id != useAuthStore().user._id">
       <!--delete and chat-->
       <div v-if="isMyFriend" class="flex flex-col sm:flex-row">
         <button
