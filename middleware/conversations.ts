08efc60ea82f5c0866563fa40f27e2343448612d
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-21 02:26:15 +0100

    await for async function

--- middleware/conversations.ts
+++ middleware/conversations.ts
@@ -5,7 +5,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const idConv = to.params.id as string;
   if (idConv) {
     const conv = await useConversationsStore().getConversation(idConv);
+
     useConversationsStore().setCurrentConversation(conv);
+    //init messages
+    await useMessagesStore().getInitialMessages();
   }
 
   //set status checking interval
