340b7465c2e985b18aa269d4323dd7f8e12d5e50
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-22 02:00:59 +0100

    bg theme color  to main section

--- layouts/conversations.vue
+++ layouts/conversations.vue
@@ -5,6 +5,19 @@
       <ConversationNavBar></ConversationNavBar>
     </div>
 
-    <div><slot /></div>
+    <div
+      :class="{
+        'bg-blue-100 text-white':
+          useConversationsStore().currentConversation.theme?._id == 'basic',
+        'bg-pink-100 text-white':
+          useConversationsStore().currentConversation.theme?._id == 'love',
+        'bg-gray-100 text-white':
+          useConversationsStore().currentConversation.theme?._id == 'panda',
+        'bg-green-100 text-white':
+          useConversationsStore().currentConversation.theme?._id == 'spring',
+      }"
+    >
+      <slot />
+    </div>
   </main>
 </template>
--- layouts/private-space.vue
+++ layouts/private-space.vue
@@ -8,6 +8,15 @@
     <div>
       <SideBar></SideBar>
     </div>
-    <div><slot /></div>
+    <div
+      :class="{
+        'bg-blue-100 text-white': useUsersStore().user.theme?._id == 'basic',
+        'bg-pink-100 text-white': useUsersStore().user.theme?._id == 'love',
+        'bg-gray-100 text-white': useUsersStore().user.theme?._id == 'panda',
+        'bg-green-100 text-white': useUsersStore().user.theme?._id == 'spring',
+      }"
+    >
+      <slot />
+    </div>
   </main>
 </template>
