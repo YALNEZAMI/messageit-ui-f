ad0027bef01a8311a2c3d56c7bfd276c476908ab
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-23 03:54:34 +0100

    globalize getDate function

--- components/message/index.vue
+++ components/message/index.vue
@@ -58,18 +58,9 @@ const thereIsImage = () => {
 const isMyMessage = () => {
   return message.sender._id == useUsersStore().user._id;
 };
-const getDate = () => {
-  const date = new Date(message.createdAt);
-
-  const day = String(date.getDate()).padStart(2, "0");
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
-  const year = date.getFullYear();
-
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-
-  return `${day}/${month}/${year} at ${hours}:${minutes}`;
-};
+function getDate() {
+  return useMessagesStore().getDate(message.createdAt);
+}
 const getConversationType = () => {
   return useConversationsStore().currentConversation.type;
 };
