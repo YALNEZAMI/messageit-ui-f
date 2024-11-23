b69d8d1770b0d0024610d54889fc1efdea60d34a
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-24 00:22:52 +0100

    style: decrese width

--- components/noResult/index.vue
+++ components/noResult/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <main class="bg-red-400 p-1 text-white h-max w-full text-center font-serif">
+  <main
+    class="bg-red-400 p-1 text-white h-max w-11/12 mx-auto text-center font-serif"
+  >
     {{ props.message }}
   </main>
 </template>
