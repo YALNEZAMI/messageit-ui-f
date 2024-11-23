1dfc608be2fa1c0f6eef0c6caed16cf37fb41bb8
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-23 01:04:14 +0100

    make a white border for status point to be visible in spring theme + add a ping effect for online users

--- components/status/index.vue
+++ components/status/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <main
-    class="w-2 h-2 rounded-full"
-    :class="{
-      'bg-gray-500': !user.onLine,
-      'bg-green-400': user.onLine,
-    }"
-  ></main>
+  <main class="w-3 h-3 rounded-full">
+    <span
+      v-if="user.onLine"
+      class="animate-ping absolute top-0 left-0 inline-flex h-full w-full rounded-full bg-sky-400 opacity-100"
+    ></span>
+    <span
+      class="relative inline-flex rounded-full h-2 w-2 border-2 border-solid border-white"
+      :class="{
+        'bg-gray-500': !user.onLine,
+        'bg-green-400': user.onLine,
+      }"
+    ></span>
+  </main>
 </template>
 <script setup>
 const props = defineProps({
