38867046899147c687a0f2bb97544adcd1f19a45
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-25 04:02:21 +0100

    display send and recieved message  status for last  currentUser message

--- components/message/index.vue
+++ components/message/index.vue
@@ -121,6 +121,30 @@
         </div>
       </div>
     </div>
+    <!--message status-->
+    <div class="flex justify-end px-2 mb-2">
+      <svg
+        v-if="isMyMessage() && isLastMessage()"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        class="bi bi-check-circle rounded-full"
+        :class="{
+          'bg-white text-black': !isRecieved,
+          'bg-blue-400': isRecieved,
+        }"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
+        />
+        <path
+          d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"
+        />
+      </svg>
+    </div>
+    <!--date-->
     <div
       class="text-center text-sm text-black"
       v-if="props.clickedId == message._id"
@@ -157,6 +181,12 @@ const reply = (msg) => {
 const getRobotImage = () => {
   return useConversationsStore().robotImage;
 };
+const isLastMessage = () => {
+  return (
+    useMessagesStore().messages[useMessagesStore().messages.length - 1]._id ==
+    message._id
+  );
+};
 const thereIsImage = () => {
   return (
     getNextMessage() == undefined ||
@@ -191,4 +221,24 @@ const getPreviousMessage = () => {
   });
   return getMessages()[i - 1];
 };
+const isRecieved = ref(false);
+onMounted(async () => {
+  if (isLastMessage()) {
+    isRecieved.value = await useMessagesStore().isRecieved(message._id);
+  }
+  //listent to recieve message to mark it as recieved
+  eventBus.on("recieving", (recieving) => {
+    if (recieving.message == message._id) {
+      isRecieved.value = true;
+    }
+  });
+  eventBus.on("userPatched", (user) => {
+    const isMember =
+      message.conversation.members.filter((mem) => mem._id == user._id).length >
+      0;
+    if (isMember) {
+      isRecieved.value = true;
+    }
+  });
+});
 </script>
