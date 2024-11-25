1f396ed556e7ec59048c740c477bfbc2d18a0ecf
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-25 04:03:39 +0100

    add an event for recievings creations

--- utils/eventBus.ts
+++ utils/eventBus.ts
@@ -1,11 +1,13 @@
 import mitt from "mitt";
 import type { Conversation } from "~/interfaces/conversation";
 import type { Message } from "~/interfaces/message";
+import type { Recieving } from "~/interfaces/recieving";
 import type { Typing } from "~/interfaces/typing";
 import type { User } from "~/interfaces/user";
 
 type Events = {
   messageReceived: Message;
+  recieving: Recieving;
   conversationChanged: Conversation;
   userPatched: User;
   refereMessage: Message;
