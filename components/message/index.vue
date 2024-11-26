2afc2fe673d24cd03993db71bd303d767fea9df0
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-26 23:49:32 +0100

    split: make a component for message status

--- components/message/index.vue
+++ components/message/index.vue
@@ -124,43 +124,7 @@
       </div>
     </div>
     <!--message status-->
-    <!--viewers images-->
-    <div
-      v-if="getViewers().length > 0 && getConversationType() != 'ai'"
-      class="flex justify-end px-2 mb-2"
-    >
-      <img
-        class="w-4 h-4 rounded-full"
-        :title="viewer.name + ' userid: ' + viewer._id"
-        :src="viewer.image"
-        v-for="viewer of getViewers()"
-        :key="viewer._id"
-      />
-    </div>
-    <div
-      class="flex justify-end px-2 mb-2"
-      v-else-if="isMyMessage() && isLastMessage()"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-check-circle rounded-full"
-        :class="{
-          'bg-white text-black': !isRecieved,
-          'bg-blue-400': isRecieved,
-        }"
-        viewBox="0 0 16 16"
-      >
-        <path
-          d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"
-        />
-        <path
-          d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"
-        />
-      </svg>
-    </div>
+    <MessageStatus :message="message"></MessageStatus>
 
     <!--date-->
     <div
@@ -239,32 +203,5 @@ const getPreviousMessage = () => {
   });
   return getMessages()[i - 1];
 };
-const isRecieved = ref(false);
-onMounted(async () => {
-  if (isLastMessage()) {
-    isRecieved.value = await useMessageStatusStore().isRecieved(message);
-  }
-  //listent to recieve message to mark it as recieved
-  eventBus.on("recieving", (recieving) => {
-    if (recieving.message == message._id) {
-      isRecieved.value = true;
-    }
-  });
-  //listent to recieve message to mark it as recieved
-  // eventBus.on("messageSeen", (messageSeen) => {
-  //   useMessageStatusStore().updateMessageViewersLocally(messageSeen);
-  // });
-  eventBus.on("userPatched", (user) => {
-    const isMember =
-      message.conversation.members.filter((mem) => mem._id == user._id).length >
-      0;
-    if (isMember) {
-      isRecieved.value = true;
-    }
-  });
-});
-const getViewers = () => {
-  const res = useMessageStatusStore().getViewers(message._id);
-  return res;
-};
+onMounted(async () => {});
 </script>
