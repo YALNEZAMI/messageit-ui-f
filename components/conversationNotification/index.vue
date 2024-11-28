1e1852102fab6881d2ad3060336d13ff5339159d
YALNEZAMI <yaseralnezami@gmail.com>
2024-11-28 11:59:05 +0100

    init message as conversation notification component

---
+++ components/conversationNotification/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="text-center text-sm text-black">
+    <div>{{ message.text }}</div>
+    <!--date-->
+    <div
+      class="text-center text-ss text-black"
+      v-if="props.clickedId == message._id"
+    >
+      {{ getDate() }}
+    </div>
+  </main>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  message: Object,
+  clickedId: String,
+});
+const message = props.message!;
+function getDate() {
+  return useMessagesStore().getDate(message.createdAt);
+}
+</script>
