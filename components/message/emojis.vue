c5a66db297adffe17a98d15164055bc6998c0792
YALNEZAMI <yaseralnezami@gmail.com>
2024-12-01 16:35:34 +0100

    make a tooltip on emoji

--- components/message/emojis.vue
+++ components/message/emojis.vue
@@ -2,13 +2,32 @@
   <main>
     <!--emojis-->
     <div class="flex">
-      <div v-for="emoji of getEmojis()" :key="emoji._id">{{ emoji.emoji }}</div>
+      <div
+        class="emoji relative cursor-pointer"
+        v-for="emoji of getEmojis()"
+        :title="getReacter(emoji).name"
+        :key="emoji._id"
+      >
+        {{ emoji.emoji }}
+        <div
+          class="title absolute hidden left-full top-0 text-sm px-1 bg-black rounded-md text-white"
+        >
+          {{ getReacter(emoji).name }}
+        </div>
+      </div>
     </div>
   </main>
 </template>
+<style scoped>
+.emoji:hover .title {
+  display: block;
+}
+</style>
+
 <script lang="ts" setup>
 import type { Emoji } from "~/interfaces/emoji";
 import type { Message } from "~/interfaces/message";
+import type { User } from "~/interfaces/user";
 
 const props = defineProps({
   message: Object,
@@ -16,10 +35,10 @@ const props = defineProps({
 const message = props.message as Message;
 const emojis = ref([] as Emoji[]);
 const getEmojis = () => {
-  return emojis.value.map((emoji: Emoji) => {
-    emoji.reacter = useConversationsStore().getMember(emoji.reacter as string);
-    return emoji;
-  });
+  return emojis.value;
+};
+const getReacter = (emoji: Emoji): User => {
+  return useConversationsStore().getMember(emoji.reacter as string);
 };
 onMounted(async () => {
   emojis.value = await useEmojisStore().getEmojis(message._id as string);
