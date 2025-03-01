e7bf717aca1054a6f5bd4ed375b5fff2bdb5a372
YALNEZAMI <yaseralnezami@gmail.com>
2025-03-01 19:53:48 +0100

    style: improve ui of user buttons in user component

--- components/user/index.vue
+++ components/user/index.vue
@@ -16,11 +16,11 @@
       {{ getUserName() }}
     </div>
     <div
-      id="btngroupe"
+      id="btngroupe flex flex-end"
       v-if="user._id != useUsersStore().user._id && !noButtons"
     >
       <!--delete and chat-->
-      <div v-if="isMyFriend" class="flex flex-col sm:flex-row">
+      <div v-if="isMyFriend" class="flex flex-col sm:flex-row flex-end">
         <button
           type="button"
           @click="remove"
@@ -64,33 +64,36 @@
           </svg>
         </button>
       </div>
-
-      <!--add-->
-
-      <button
-        type="button"
-        class="userButton bg-indigo-500 hover:bg-indigo-600"
+      <div
+        class="flex flex-end"
         v-else-if="
           !isMyFriend && !isISentFriendRequest && !isHeSentFriendRequest
         "
-        @click="addFriendRequest"
       >
-        <div>Ajouter</div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="size-4"
+        <!--add-->
+        <button
+          type="button"
+          class="userButton bg-indigo-500 hover:bg-indigo-600"
+          @click="addFriendRequest"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-          />
-        </svg>
-      </button>
+          <div>Ajouter</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-4"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+            />
+          </svg>
+        </button>
+      </div>
+
       <!--cancel-->
       <button
         type="button"
@@ -114,9 +117,11 @@
           />
         </svg>
       </button>
-      <div v-else-if="isHeSentFriendRequest" class="flex flex-col sm:flex-row">
+      <div
+        v-else-if="isHeSentFriendRequest"
+        class="flex flex-col sm:flex-row flex-end"
+      >
         <!--accept-->
-
         <button
           @click="accept"
           type="button"
@@ -139,7 +144,6 @@
           </svg>
         </button>
         <!--refuse-->
-
         <button
           type="button"
           class="userButton bg-red-500 hover:bg-red-600"
