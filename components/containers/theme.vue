7ad7d4ebe3001a2540c5ef0f8bb1ad40749e4904
YALNEZAMI <yaseralnezami@gmail.com>
2025-03-01 20:47:42 +0100

    style: update theme colors for basic theme

--- components/containers/theme.vue
+++ components/containers/theme.vue
@@ -2,7 +2,7 @@
 <template>
   <div
     :class="{
-      'bg-blue-400 text-white': useUsersStore().user.theme?._id == 'basic',
+      'bg-white text-black': useUsersStore().user.theme?._id == 'basic',
       'bg-pink-400 text-white': useUsersStore().user.theme?._id == 'love',
       'bg-black text-white': useUsersStore().user.theme?._id == 'panda',
       'bg-green-400 text-white': useUsersStore().user.theme?._id == 'spring',
