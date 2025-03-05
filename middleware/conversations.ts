fb6640494ccd778552ca29f28955c90b1ae8edf7
YALNEZAMI <yaseralnezami@gmail.com>
2025-03-05 11:42:21 +0100

    fix: prevent access to conversations user is not a member of

--- middleware/conversations.ts
+++ middleware/conversations.ts
@@ -1,4 +1,5 @@
 import { eventBus } from "@/utils/eventBus";
+import type { User } from "~/interfaces/user";
 import { useMessageStatusStore } from "~/stores/messageStatus.store";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
@@ -8,6 +9,14 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const idConv = to.params.id as string;
   if (idConv) {
     const conv = await useConversationsStore().getConversation(idConv);
+    console.log("conv", conv);
+    const isMember =
+      conv.members.filter(
+        (member: User) => member._id == useUsersStore().user._id
+      ).length > 0;
+    if (!isMember) {
+      useRouter().push("/conversations");
+    }
     await useConversationsStore().setCurrentConversation(conv);
 
     //init messages
