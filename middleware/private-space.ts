7ab2ed1cf4cbb7045161ce88f101bf2b5841cd19
YALNEZAMI <yaseralnezami@gmail.com>
2025-03-21 00:52:17 +0100

    refactor: remove unnecessary code in private-space middleware

--- middleware/private-space.ts
+++ middleware/private-space.ts
@@ -1,7 +1,4 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
-
-  //set me as connected
-  useUsersStore().setStatusCheckingIntervalle();
 });
