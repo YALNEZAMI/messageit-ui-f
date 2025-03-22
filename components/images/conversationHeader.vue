101b16a8155c79851d61f8df695d5b679904f810
YALNEZAMI <yaseralnezami@gmail.com>
2025-03-22 02:53:23 +0100

    fix: handle undefined image in conversation header

--- components/images/conversationHeader.vue
+++ components/images/conversationHeader.vue
@@ -9,27 +9,28 @@
 </template>
 
 <script lang="ts" setup>
-const getConversationImage = () => {
+const getConversationImage = (): string => {
   const conversation = useConversationsStore().currentConversation;
   switch (conversation.type) {
     case "ai":
       return useConversationsStore().robotImage;
     case "group":
-      return conversation.image;
+      return conversation.image!;
     case "private":
       return useConversationsStore().getOtherUser(conversation).image;
     default:
+      return useConversationsStore().getOtherUser(conversation).image;
       break;
   }
 };
 
 const authStore = useAuthStore();
-const defaultUserImg = authStore.defaultUserImg;
 
 const imgSrc = ref(""); // Local reactive variable for the image source
 
 const handleError = () => {
-  imgSrc.value = defaultUserImg; // Set to default image on error
+  imgSrc.value =
+    "https://cdn.pixabay.com/photo/2023/02/18/11/00/icon-7797704_1280.png"; // Set to default image on error
 };
 onMounted(async () => {
   imgSrc.value = getConversationImage();
