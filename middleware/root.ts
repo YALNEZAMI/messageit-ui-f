3abf8fc81371f92e8bf8459f3515ddeed573fb64
YALNEZAMI <yaseralnezami@gmail.com>
2025-05-13 02:37:02 +0200

    feat: redirect unauthenticated users to login page

--- middleware/root.ts
+++ middleware/root.ts
@@ -1,7 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   //to is the destination path(object)
   //from is the origine path(object)
-  if (to.fullPath == "/") {
+  const isAuthenticated = useAuthStore().isAuthenticated();
+  if (isAuthenticated) {
+    return navigateTo("/conversations");
+  } else {
     return navigateTo("/auth/login");
   }
 });
